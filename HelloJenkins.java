6b51a982f6fc59c85ea81eb2b21e1ffac9ff3a77
Ashuoles3707 <ashuole501@gmail.com>
2025-05-29 13:24:00 +0530

    Update HelloJenkins.java
    commit

--- HelloJenkins.java
+++ HelloJenkins.java
@@ -1,5 +1,5 @@
 public class HelloJenkins {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins running Java!");
+        System.out.println("Hello from Jenkins running Java! ashu");
     }
 }
